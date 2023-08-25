000621727f111ab4d8e8ccbde7e6814e2a1efbef
JatinPrakash070420 <jatin.narain@productfy.io>
2023-08-25 18:40:14 +0530

    introducing kafka producer, for sending the message

--- mathmagic/src/main/java/Sprithink/mathmagic/MultiplicationApplication/service/Impl/MultiplicationServiceImpl.java
+++ mathmagic/src/main/java/Sprithink/mathmagic/MultiplicationApplication/service/Impl/MultiplicationServiceImpl.java
@@ -9,11 +9,14 @@ import io.leangen.graphql.annotations.GraphQLArgument;
 import io.leangen.graphql.annotations.GraphQLMutation;
 import io.leangen.graphql.annotations.GraphQLQuery;
 import io.leangen.graphql.spqr.spring.annotations.GraphQLApi;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.Properties;
 
 @Service
 @GraphQLApi
@@ -32,6 +35,18 @@ public class MultiplicationServiceImpl implements MultiplicationService {
     @Override
     public boolean checkMultiplication(@GraphQLArgument(name = "Id", description = "Id of the multiplication object")int id,
                                        @GraphQLArgument(name = "supplyProduct", description = "value of the Multiplication") int supplyProduct) {
+        //Setting up the properties in kafka
+        Properties kafkaProps = new Properties();
+        kafkaProps.put("bootstrap.servers","localhost:9092");
+        kafkaProps.put("key.serializer","org.apache.kafka.common.serialization.StringSerializer");
+        kafkaProps.put("value.serializer",
+                "org.apache.kafka.common.serialization.StringSerializer");
+        //creating a producer using the same properties
+        KafkaProducer producer = new KafkaProducer<>(kafkaProps);
+
+        //Sending the message
+        ProducerRecord<String,String> producerRecord = new ProducerRecord<>("microservices-test","test","passed");
+        producer.send(producerRecord);
         Optional<Multiplication> optionalMultiplication = multiplyRepo.findById(id);
         Multiplication multiplication = optionalMultiplication.get();
         int actualProduct = multiplication.mult1 * multiplication.mult2;
